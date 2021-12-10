f9cd74f2626d2f072e01eeae8c194071c7b1cd58
kevin.talmadge@eluv.io <kevin@eluv.io>
2021-12-09 23:10:31 -0800

    Hide bottom navigation if marketplace is not set

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,14 @@
 import React from "react";
 import {observer} from "mobx-react";
+import {rootStore} from "Stores";
 import {NavLink} from "react-router-dom";
 
 const Navigation = observer(() => {
-  if(!rootStore.loggedIn || rootStore.hideNavigation) { return null; }
+  if(!rootStore.loggedIn || rootStore.hideNavigation || !rootStore.marketplaceId) { return null; }
 
   return (
     <nav className="navigation">
-      {
-        rootStore.marketplaceId ?
-          <NavLink className="navigation__link" to={`/marketplaces/${rootStore.marketplaceId}`}>Marketplace</NavLink> :
-          <NavLink className="navigation__link" to="/marketplaces">Discover</NavLink>
-      }
+      <NavLink className="navigation__link" to={`/marketplaces/${rootStore.marketplaceId}`}>Marketplace</NavLink>
       <NavLink className="navigation__link" to="/wallet">Wallet</NavLink>
     </nav>
   );
