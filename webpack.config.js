782e426da61872a0cb541e4f5ded6ed60d90a081
kevin.talmadge@eluv.io <kevin@eluv.io>
2022-04-17 13:28:03 -0700

    Fix dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -90,7 +90,7 @@ module.exports = {
               plugins: () => [autoprefixer({})]
             }
           },
-          "fast-sass-loader"
+          "sass-loader"
         ]
       },
       {
