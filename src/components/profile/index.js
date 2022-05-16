62a2c781807d91fabb37c9ede4126d1ee37ad265
kevin.talmadge@eluv.io <kevin@eluv.io>
2022-05-16 10:20:51 -0700

    Update funds copy to specify new 15 day withdrawal period

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -79,7 +79,7 @@ const WithdrawalDetails = observer(({setShowWithdrawalModal, setShowWithdrawalSe
       }
 
       <div className="profile-page__message">
-        Funds availability notice – A hold period will be imposed on amounts that accrue from the sale of an NFT. Account holders acknowledge that, during this hold period, a seller will be unable to use or withdraw the amounts attributable to such sale(s).  The current hold period for spending the balance is 7 days, and withdrawing the balance is 30 days.
+        Funds availability notice – A hold period will be imposed on amounts that accrue from the sale of an NFT. Account holders acknowledge that, during this hold period, a seller will be unable to use or withdraw the amounts attributable to such sale(s).  The current hold period for spending the balance is 7 days, and withdrawing the balance is 15 days.
       </div>
       <div className="profile-page__message">
         For questions or concerns, please contact <a href={"mailto:payments@eluv.io"}>payments@eluv.io</a>
