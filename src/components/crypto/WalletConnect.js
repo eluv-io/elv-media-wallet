afeb6fd78833aff5b766bb2b492346e58e56074d
Todd Hodes <todd.hodes@eluv.io>
2023-04-06 16:35:11 -0700

    point to USDC for the address

--- src/components/crypto/WalletConnect.js
+++ src/components/crypto/WalletConnect.js
@@ -164,7 +164,7 @@ const WalletConnect = observer(({type="phantom", showPaymentPreference, onConnec
                       { "account: " + rootStore.cryptoStore.CircleAddress() }
                     </div>
                     <div className="wallet-connect__help-link">
-                      address:&nbsp;<a href= {"https://goerli.etherscan.io/address/" + rootStore.cryptoStore.CircleLinkedAddress()}  target="_blank" rel="noopener">
+                      address:&nbsp;<a href= {"https://goerli.etherscan.io/token/0x07865c6e87b9f70255377e024ace6630c1eaa37f?a=" + rootStore.cryptoStore.CircleLinkedAddress()}  target="_blank" rel="noopener">
                         {rootStore.cryptoStore.CircleLinkedAddress()}</a>
                     </div>
                   </div> : connectedAccount.link_acct
