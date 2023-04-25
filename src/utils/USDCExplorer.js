d76187ac489c7cf665b17e4ccfa4779cc647301e
Todd Hodes <todd.hodes@eluv.io>
2023-04-24 17:27:21 -0700

    cleanup

--- src/utils/USDCExplorer.js
+++ src/utils/USDCExplorer.js
@@ -1,9 +1,10 @@
 
 const USDCBlockExplorerUrl = (addr) => {
-  const mainTokenExplorer = EluvioConfiguration.usdcExplorer || "https://etherscan.io/token/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-  const testTokenExplorer = EluvioConfiguration.usdcExplorer || "https://goerli.etherscan.io/token/0x07865c6e87b9f70255377e024ace6630c1eaa37f";
+  const mainTokenExplorer = "https://etherscan.io/token/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+  const testTokenExplorer = "https://goerli.etherscan.io/token/0x07865c6e87b9f70255377e024ace6630c1eaa37f";
 
-  return EluvioConfiguration.network === "demo" ? testTokenExplorer + "?a=" + addr : mainTokenExplorer + "?a=" + addr;
+  const explorer = EluvioConfiguration.usdcExplorer || EluvioConfiguration.network === "demo" ? testTokenExplorer : mainTokenExplorer;
+  return explorer + "?a=" + addr;
 };
 
 export default USDCBlockExplorerUrl;
