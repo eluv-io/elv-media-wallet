3e6529b61b25e650d777fd3e2b6064a810aa942f
kevin.talmadge@eluv.io <kevin@eluv.io>
2023-06-26 11:13:31 -0700

    Add support for media reference type

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -118,13 +118,15 @@ export const ScrollTo = (top=0, target) => {
   // Don't scroll to 0 by default, it will cause the header to un-minimize. Should only scroll to 0 if the page is already scrolled to 0.
   if(!target) {
     top = Math.max(top, Math.min(window.scrollY, 1));
+  } else {
+    top = target.getBoundingClientRect().top + window.scrollY + top;
   }
 
   // Mobile has a bug that prevents scroll top from working
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    (target || window).scrollTo(0, top);
+    window.scrollTo(0, top);
   } else {
-    (target || window).scrollTo({top, behavior: "smooth"});
+    window.scrollTo({top, behavior: "smooth"});
   }
 };
 
