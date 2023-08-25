8878cdf71c855a69dffc599dbad4d9da9c6c8812
Todd Hodes <todd.hodes@eluv.io>
2023-08-25 16:03:50 -0700

    rename

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     path: Path.resolve(__dirname, "dist"),
     chunkFilename: "[name].bundle.js",
     clean: true,
-    filename: 'index.js'
+    filename: 'idx.js'
   },
   devServer: {
     hot: true,
