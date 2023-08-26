39d970aaffe3b033499f530e16bc48a2ef917f13
kevin.talmadge@eluv.io <kevin@eluv.io>
2023-08-25 18:54:25 -0700

    Add small delay to SetCustomCSS to account for location changing

--- src/stores/index.js
+++ src/stores/index.js
@@ -848,6 +848,7 @@ class RootStore {
   });
 
   SetCustomCSS(css="") {
+    console.log(new Error("Set custom CSS " + css));
     css = SanitizeHTML(css);
     const cssTag = document.getElementById("_custom-css");
     if(cssTag) {
@@ -968,11 +969,12 @@ class RootStore {
         }
       }
 
-      this.SetCustomizationOptions(marketplace);
-
       if(marketplace?.default_display_currency) {
         this.checkoutStore.SetCurrency({currency: marketplace?.default_display_currency});
       }
+
+      // Give locationType time to settle if path changed
+      setTimeout(() => this.SetCustomizationOptions(marketplace), 100);
 
       return marketplace.marketplaceHash;
     } else if(Object.keys(this.walletClient.availableMarketplaces) > 0) {
