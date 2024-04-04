1be3ea0687b2092e37ea63c0e680aa044756f3fa
kevin.talmadge@eluv.io <kevin@eluv.io>
2024-04-04 15:44:05 -0700

    Fix padding on page

--- src/components/properties/MediaPropertyPage.jsx
+++ src/components/properties/MediaPropertyPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {observer} from "mobx-react";
 import {mediaPropertyStore} from "Stores";
 import {Redirect, useRouteMatch} from "react-router-dom";
