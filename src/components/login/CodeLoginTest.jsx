0dfeb159a6d8e66c21ad80cfc23e2dbe4b0acf30
kevin.talmadge@eluv.io <kevin@eluv.io>
2025-10-23 10:31:21 -0700

    Update code login test to use install ID

--- src/components/login/CodeLoginTest.jsx
+++ src/components/login/CodeLoginTest.jsx
@@ -3,10 +3,15 @@ import {observer} from "mobx-react";
 import {rootStore} from "Stores";
 import {Linkish, QRCodeElement} from "Components/common/UIComponents";
 import {SHA512} from "../../utils/Utils";
+import {Button} from "Components/properties/Common";
+import {ElvClient, ElvWalletClient} from "@eluvio/elv-client-js";
+
+const nonce = "TESTNONCE";
 
 const CodeLoginTest = observer(() => {
   const [codeInfo, setCodeInfo] = useState(undefined);
   const [state, setState] = useState(undefined);
+  const [client, setClient] = useState(undefined);
 
   // Generate code
   useEffect(() => {
@@ -16,7 +21,7 @@ const CodeLoginTest = observer(() => {
         url.searchParams.set("pid", rootStore.currentPropertySlug);
         url.searchParams.set("clear", "");
         //url.searchParams.set("nonce", "TESTNONCE");
-        url.searchParams.set("installId", await SHA512("TESTNONCE"));
+        url.searchParams.set("installId", await SHA512(nonce));
         url.searchParams.set("origin", "CODE TEST LOGIN");
 
         setCodeInfo({...response, url: url.toString()});
@@ -37,6 +42,30 @@ const CodeLoginTest = observer(() => {
 
       const authInfo = JSON.parse(response.payload);
       setState(authInfo);
+
+      const client = await ElvClient.FromConfigurationUrl({
+        configUrl:
+          EluvioConfiguration.network === "main" ?
+            "https://main.glb.contentfabric.io/s/main/config" :
+            "https://demov3.net955210.contentfabric.io/config"
+      });
+
+      const walletClient = await ElvWalletClient.Initialize({
+        client,
+        appId: rootStore.appId,
+        network: EluvioConfiguration.network,
+        mode: EluvioConfiguration.mode,
+        storeAuthToken: false,
+        skipMarketplaceLoad: true
+      });
+
+      await walletClient.Authenticate({
+        token: client.utils.B58(JSON.stringify(authInfo))
+      });
+
+      client.walletClient = walletClient;
+
+      setClient(client);
 
       // eslint-disable-next-line no-console
       console.log(authInfo);
@@ -71,6 +100,23 @@ const CodeLoginTest = observer(() => {
           <div className="code-test__message">Login Successful!</div>
           <div className="code-test__message">{state.email}</div>
         </div>
+        <Button
+          onClick={async () => {
+            setState(
+              await client.signer.RefreshCSAT({
+                accessToken: state.token,
+                refreshToken: state.refreshToken || state.refresh_token,
+                nonce
+              })
+            );
+          }}
+          className="code-test__refresh"
+        >
+          Refresh
+        </Button>
+        <pre className="code-test__response">
+          { JSON.stringify(state, null, 2) }
+        </pre>
       </div>
     );
   }
